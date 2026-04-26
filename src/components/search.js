eb37eda746f2e4dce3cc6ad5fca04d1368aa2dc3
Arunkumar1321 <akmirror619@gmail.com>
2026-04-26 07:32:44 +0530

    All files are uploaded

---
+++ src/components/search.js
@@ -0,0 +1,10 @@
+// Search Component
+function Search(){
+    return(
+        <div className="searchbar">
+            <input type="text" placeholder="Search for products"></input>
+
+        </div>
+    )
+}
+export default Search
